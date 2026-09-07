f553196dae6a3c06fe05eb1d217678592748551f
FuadChy <fuadchy2020@gmail.com>
2026-09-07 22:18:24 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-5xl text-red-500">Bpl Dream 11</h1>
+      <button className="btn btn-error">Error</button>
+    </>
+  )
+}
+
+export default App
